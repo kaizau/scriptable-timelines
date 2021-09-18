89ef2a9bf4a33b07673c9db7a499b4a54154d7fc
Kai Zau <k@kaizau.com>
2021-09-18 12:16:52 +0800

    Initial restored

---
+++ README.md
@@ -0,0 +1,12 @@
+# Timelines: A [Scriptable](https://scriptable.app/) Widget
+
+An iOS widget to show your calendar as a set of finite timelines and your age
+to the 1000th digit.
+
+Definitely not for everyone. 😂
+
+<img src="thumbnail.webp" width="400">
+
+---
+
+Inspired by [Juniorchen2012's progress.js](https://github.com/Juniorchen2012/scriptable/blob/master/progress.js).
---
+++ Timelines.js
@@ -0,0 +1,298 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: calendar;
+
+//
+// Config
+//
+
+const birthday = new Date(1970, 0, 1); // Month starts with 0: 0 => Jan, 1 => Feb, 11 => Dec
+const estimatedLifespan = 75;
+
+const hideDefaultHolidayCalendar = true;
+const calendarUrl = "fantastical://";
+
+const enableNote = false;
+const noteFileBookmark = "Obsidian Daily Note";
+const noteFileExtension = "md";
+const noteUrl = "obsidian://";
+
+//
+// Widget + Layout
+//
+
+const stackSpacing = enableNote ? 10 : 15;
+const timelineWidth = enableNote ? 50 : 70;
+const timelineHeight = 4;
+const calendarWidth = 115;
+const noteWidth = 115;
+const colorBackground = Color.dynamic(
+  new Color("#ffffff", 0.25),
+  new Color("#222222", 0.25)
+);
+const colorText = Color.dynamic(new Color("#222222"), new Color("#ffffff"));
+const now = new Date();
+const format = new DateFormat();
+
+const widget = new ListWidget();
+widget.backgroundColor = colorBackground;
+const wrapper = widget.addStack();
+wrapper.spacing = stackSpacing;
+
+const timelineCol = addColumn(wrapper);
+timelineCol.size = new Size(timelineWidth, 0);
+createTimeline(timelineCol);
+
+const calendarCol = addColumn(wrapper);
+await createCalendar(calendarCol);
+
+if (enableNote) {
+  calendarCol.size = new Size(calendarWidth, 0);
+  const noteCol = addColumn(wrapper);
+  noteCol.size = new Size(noteWidth, 0);
+  createDailyNote(noteCol);
+}
+
+Script.setWidget(widget);
+Script.complete();
+widget.presentMedium();
+
+//
+// Util
+//
+
+function addColumn(parent) {
+  const col = parent.addStack();
+  col.layoutVertically();
+  return col;
+}
+
+function DateFormat() {
+  this.dayOfWeek = new DateFormatter();
+  this.dayOfWeek.dateFormat = "EEE";
+
+  this.month = new DateFormatter();
+  this.month.dateFormat = "MMM";
+
+  this.date = new DateFormatter();
+  this.date.useShortDateStyle();
+  this.date.useNoTimeStyle();
+
+  this.time = new DateFormatter();
+  this.time.useNoDateStyle();
+  this.time.useShortTimeStyle();
+
+  this.timestamp = new DateFormatter();
+  this.timestamp.dateFormat = "yyyy-MM-dd";
+}
+
+//
+// Timelines
+//
+
+function createTimeline(stack) {
+  stack.url = calendarUrl;
+
+  createTimelineItem(stack, 7, now.getDay() + 1, format.dayOfWeek.string(now));
+  createTimelineItem(
+    stack,
+    24 * 60,
+    (now.getHours() + 1) * 60 + now.getMinutes(),
+    ordinalize(now.getDate())
+  );
+  createTimelineItem(
+    stack,
+    daysInMonth(now.getMonth() + 1, now.getFullYear()),
+    now.getDate(),
+    format.month.string(now)
+  );
+  createTimelineItem(
+    stack,
+    12,
+    now.getMonth() + 1,
+    now.getFullYear().toString()
+  );
+
+  let lastBirthday = new Date(
+    now.getFullYear(),
+    birthday.getMonth(),
+    birthday.getDate()
+  );
+  if (
+    lastBirthday > now &&
+    !(
+      now.getMonth() === birthday.getMonth() &&
+      now.getDate() === birthday.getDate()
+    )
+  ) {
+    lastBirthday.setFullYear(now.getFullYear() - 1);
+  }
+  const msSinceLastBirthday = now - lastBirthday;
+  const daysThisYear = isLeapYear(now.getFullYear()) ? 366 : 365;
+  const fractionalAge =
+    msSinceLastBirthday / (daysThisYear * 24 * 60 * 60 * 1000);
+  const diffYears =
+    Math.abs(new Date(now - birthday).getUTCFullYear() - 1970) + fractionalAge;
+  createTimelineItem(stack, estimatedLifespan, diffYears, diffYears.toFixed(3));
+}
+
+function ordinalize(num) {
+  if (num === 1) {
+    return "1st";
+  } else if (num === 2) {
+    return "2nd";
+  } else {
+    return num + "th";
+  }
+}
+
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+function isLeapYear(year) {
+  return new Date(year, 1, 29).getDate() === 29;
+}
+
+function createTimelineItem(stack, total, elapsed, label) {
+  const title = stack.addText(label);
+  title.textColor = colorText;
+  title.font = Font.systemFont(11);
+  stack.addSpacer(1);
+  const img = stack.addImage(createProgressBar(total, elapsed));
+  img.imageSize = new Size(timelineWidth, timelineHeight);
+  stack.addSpacer(4);
+}
+
+function createProgressBar(total, elapsed) {
+  const context = new DrawContext();
+  context.size = new Size(timelineWidth, timelineHeight);
+  context.opaque = false;
+  context.respectScreenScale = true;
+  context.setFillColor(new Color("#48484b"));
+  const path = new Path();
+  path.addRoundedRect(new Rect(0, 0, timelineWidth, timelineHeight), 2, 2);
+  context.addPath(path);
+  context.fillPath();
+  context.setFillColor(new Color("#ffd60a"));
+  const path1 = new Path();
+  path1.addRoundedRect(
+    new Rect(0, 0, (timelineWidth * elapsed) / total, timelineHeight),
+    2,
+    2
+  );
+  context.addPath(path1);
+  context.fillPath();
+  return context.getImage();
+}
+
+//
+// Calendar
+//
+// NOTE: Using magic numbers and &nbsp; for spacing.
+//
+
+async function createCalendar(stack) {
+  stack.url = calendarUrl;
+
+  const events = await CalendarEvent.today();
+  events
+    .filter((event) => {
+      if (
+        hideDefaultHolidayCalendar &&
+        event.calendar.isSubscribed &&
+        !event.calendar.allowsContentModifications &&
+        event.calendar.title.match(/^\w+ Holidays$/)
+      ) {
+        return false;
+      }
+      return true;
+    })
+    .filter((event) => event.endDate > now)
+    .slice(0, 4)
+    .forEach((event) => createCalendarEvent(stack, event));
+}
+
+function createCalendarEvent(stack, event) {
+  const item = stack.addStack();
+  item.centerAlignContent();
+
+  // All day event
+  if (event.isAllDay) {
+    const symbol = item.addText("\u258D");
+    symbol.font = Font.systemFont(9);
+    symbol.textColor = event.calendar.color;
+
+    const eventTitle = item.addText(event.title);
+    eventTitle.textColor = colorText;
+    eventTitle.font = Font.systemFont(11);
+  }
+
+  // Timed event
+  else {
+    const symbol = item.addText("\u2B24  ");
+    symbol.font = Font.systemFont(4);
+    symbol.textColor = event.calendar.color;
+
+    let dateString = "";
+    const todayDate = format.date.string(now);
+    const startDate = format.date.string(event.startDate);
+    const endDate = format.date.string(event.endDate);
+    if (startDate === todayDate) {
+      dateString += format.time.string(event.startDate) + "-";
+    } else {
+      dateString += "ends ";
+    }
+    if (endDate !== todayDate) {
+      dateString += format.dayOfWeek.string(event.endDate) + " ";
+    }
+    dateString += format.time.string(event.endDate);
+
+    const eventTime = item.addText(dateString);
+    eventTime.textColor = colorText;
+    eventTime.font = Font.systemFont(9);
+
+    // Add directly to calendar stack to avoid making layout more complex
+    const eventDetails = stack.addStack();
+    eventDetails.setPadding(0, 8, 0, 0);
+    const eventTitle = eventDetails.addText(event.title);
+    eventTitle.textColor = colorText;
+    eventTitle.font = Font.systemFont(11);
+  }
+
+  stack.addSpacer(4);
+}
+
+//
+// Daily Notes
+//
+
+function createDailyNote(stack) {
+  stack.url = noteUrl;
+
+  const fm = FileManager.iCloud();
+  const rootPath = fm.bookmarkedPath(noteFileBookmark);
+  const notePath = `${rootPath}/${format.timestamp.string(
+    now
+  )}.${noteFileExtension}`;
+
+  let notes;
+  if (fm.fileExists(notePath)) {
+    const todo = "- [ ] ";
+    notes = fm
+      .readString(notePath)
+      .split("\n")
+      .filter((line) => line.startsWith(todo) && line !== todo)
+      .map((line) => line.replace(todo, "⧠ "))
+      .slice(0, 4);
+  } else {
+    notes = ["No daily note."];
+  }
+
+  notes.forEach((note) => {
+    const text = stack.addText(note);
+    text.textColor = colorText;
+    text.font = Font.regularMonospacedSystemFont(11);
+    stack.addSpacer(4);
+  });
+}
